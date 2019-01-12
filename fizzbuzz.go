15eedd6b56e569200e8bb60458ae3247da638f18
supagrid <boyisboyis.work@gmail.com>
2019-01-12 14:41:27 +0700

    implement fizzbuz

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -1,6 +1,14 @@
 package fizzbuzz
 
-func Say(n int) string {
-	return "1"
-}
+import "strconv"
 
+//Say ...
+func Say(n int) string {
+	if n == 3 {
+		return "Fizz"
+	}
+	if n == 5 {
+		return "Buzz"
+	}
+	return strconv.Itoa(n)
+}
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -16,4 +16,44 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("it should say %s but get %s", get, want)
 		}
 	})
+
+	t.Run("2 should say 2", func(t *testing.T) {
+		get := fizzbuzz.Say(2)
+
+		want := "2"
+
+		if get != want {
+			t.Errorf("it should say %s but get %s", get, want)
+		}
+	})
+
+	t.Run("3 should say Fizz", func(t *testing.T) {
+		get := fizzbuzz.Say(3)
+
+		want := "Fizz"
+
+		if get != want {
+			t.Errorf("it should say %s but get %s", get, want)
+		}
+	})
+
+	t.Run("4 should say 4", func(t *testing.T) {
+		get := fizzbuzz.Say(4)
+
+		want := "4"
+
+		if get != want {
+			t.Errorf("it should say %s but get %s", get, want)
+		}
+	})
+
+	t.Run("5 should say Buzz", func(t *testing.T) {
+		get := fizzbuzz.Say(5)
+
+		want := "Buzz"
+
+		if get != want {
+			t.Errorf("it should say %s but get %s", get, want)
+		}
+	})
 }
